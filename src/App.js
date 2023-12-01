3e89a8d3ce4a39ad10837c4f3ce88e496b63f0d9
meryemkutlu <meryem.kutlu13@gmail.com>
2023-12-01 11:23:42 +0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-function App() {
-  return (
-    <div className="App">
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
 
-    </div>
+function App() {
+  const [holidays, setHolidays] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>National Holidays Dates</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div key={holiday.id} className="holiday">
+              <img src={holiday.img} />
+              <div>
+                <p>{holiday.title}</p>
+                <p>{holiday.date}</p>
+              </div>
+            </div>
+          );
+        })}
+        <button onClick={() => setHolidays([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
